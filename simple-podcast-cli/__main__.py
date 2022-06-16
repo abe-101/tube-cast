bcf996a78ebc3a51688c08b466731062cc7d7ce8
Abe <abe@habet.dev>
2022-06-16 17:59:17 -0400

    add id conversion

--- simple-podcast-cli/__main__.py
+++ simple-podcast-cli/__main__.py
@@ -5,18 +5,22 @@ from my_scrapetube import return_channel, return_playlist
 
 @click.group()
 def cli():
+    """Convert YouTube video(s) to Anchor FM"""
     pass
 
 @cli.command()
 @click.argument('ids', nargs=-1)
 def youtube_id(ids):
+    """Takes in YouTube IDs as arguments"""
     click.echo(f'Converting Youtube Video to Anchor FM')
     ids = list(ids)
     click.echo(ids)
+    convert_podcast(ids)
 
 @cli.command()
 @click.argument('filename', type=click.Path(exists=True))
 def youtube_id_from_file(filename):
+    """Takes in a file containing youtube id (one per line)"""
     click.echo(f'Converting Youtube Video (from file) to Anchor FM')
     ids = []
     with open(filename) as file:
@@ -26,10 +30,12 @@ def youtube_id_from_file(filename):
 
 @cli.command()
 def youtube_channel():
+    """Takes in a YouTube Channel ID"""
     click.echo('Converting YouTube Channel to Anchor FM')
 
 @cli.command()
 def youtube_playlist():
+    """Take in a YouTube Playlist ID"""
     click.echo('Converting YouTube Playlist to Anchor FM')
 
 if __name__ == '__main__':
