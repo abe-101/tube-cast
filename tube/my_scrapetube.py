e44d7191d511828fad3709b423465ffd58a42a14
Abe <abe@habet.dev>
2022-06-30 16:02:40 -0400

    catch invalid error

--- tube/my_scrapetube.py
+++ tube/my_scrapetube.py
@@ -2,16 +2,22 @@ import scrapetube
 
 
 def return_channel(id: str):
-    videos = scrapetube.get_channel(id)
-    result = []
-    for video in videos:
-        result.append(video["videoId"])
-    return result
+    try:
+        videos = scrapetube.get_channel(id)
+        result = []
+        for video in videos:
+            result.append(video["videoId"])
+        return result
+    except:
+        exit("Invalid channel id")
 
 
 def return_playlist(id: str):
-    videos = scrapetube.get_playlist(id)
-    result = []
-    for video in videos:
-        result.append(video["videoId"])
-    return result
+    try:
+        videos = scrapetube.get_playlist(id)
+        result = []
+        for video in videos:
+            result.append(video["videoId"])
+        return result
+    except:
+        exit("Invalid playlist id")
