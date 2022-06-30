22b0ac96daad501ec88cf49aa9aca6904f580eeb
Abe <abe@habet.dev>
2022-06-30 17:40:50 -0400

    test my scrapetube

---
+++ test/test_my_scrapetube.py
@@ -0,0 +1,19 @@
+import pytest
+from tube.my_scrapetube import return_channel, return_playlist
+
+def test_return_channel():
+    with pytest.raises(SystemExit) as e:
+        return_channel("123")
+    assert e.type == SystemExit
+    assert e.value.code == "Invalid channel id"
+
+
+def test_return_playlist():
+    with pytest.raises(SystemExit) as e:
+        return_playlist("232")
+    assert e.type == SystemExit
+    assert e.value.code == "Invalid playlist id"
+
+if __name__ == '__main__':
+    test_exit()
+
