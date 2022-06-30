f449a2497dfe585251ef6a8524fc8e2a36dc15d2
Abe <abe@habet.dev>
2022-06-30 15:09:50 -0400

    proccess channel and playlist

--- tube/__main__.py
+++ tube/__main__.py
@@ -2,24 +2,25 @@ import sys
 import os
 import click
 import getpass
+
 from validate_id import valid_id
 from my_scrapetube import return_channel, return_playlist
 from youtube_to_anchorFM import convert_youtube_to_podcast
 
 @click.group()
 @click.option("-d", "--draft-mode", is_flag=True, help="Save podcast as draft")
-@click.option('-t', '--thumbnail', is_flag=True, help="Include YouTube thumbnail in podcast")
-@click.option('-u', '--add-url', is_flag=True, help="Add YouTube url to podcast description")
+@click.option('-t', '--thumbnail_mode', is_flag=True, help="Include YouTube thumbnail in podcast")
+@click.option('-u', '--add-url', is_flag=True, help="Add the YouTube URL To podcast description")
 @click.option('-x', '--is-explicit', is_flag=True, help="Mark podcast as explicit")
 @click.pass_context
-def cli(ctx, draft_mode, thumbnail, add_url, is_explicit):
+def cli(ctx, draft_mode, thumbnail_mode, add_url, is_explicit):
     """Convert YouTube video(s) to Anchor FM"""
     ctx.ensure_object(dict)
 
-    ctx.obj['DRAFT_MODE'] = draft_mode
-    ctx.obj['THUMBNAIL'] = thumbnail
-    ctx.obj['ADD_URL'] = add_url
-    ctx.obj['IS_EXPLICIT'] = is_explicit
+    ctx.obj['draft_mode'] = draft_mode
+    ctx.obj['thumbnail_mode'] = thumbnail_mode
+    ctx.obj['url_in_description'] = add_url
+    ctx.obj['is_explicit'] = is_explicit
 
     if not os.getenv("ANCHOR_EMAIL") or not os.getenv("ANCHOR_PASSWORD"):
         os.environ["ANCHOR_EMAIL"] = input("Enter anchor.FM user email: ")
@@ -32,36 +33,39 @@ def cli(ctx, draft_mode, thumbnail, add_url, is_explicit):
 def youtube_id(ctx, ids):
     """Takes in YouTube ID(s) as arguments"""
     click.echo(f'Converting Youtube Video to Anchor FM')
-    click.echo(f'draft_value: {ctx.obj}')
-
     ids = list(ids)
-    click.echo(ids)
-    for id in ids:
-        if valid_id(id):
-            convert_youtube_to_podcast(id, ctx.obj)
-        else:
-            print("Id: " + id + " is Invalid - Skipping")
+    convert_youtube_to_podcast(ids, **ctx.obj)
 
 @cli.command()
+@click.pass_context
 @click.argument('filename', type=click.Path(exists=True))
-def youtube_id_from_file(filename, draft_mode, thumbnail, add_url, is_explicit):
+def youtube_id_from_file(ctx, filename):
     """Takes in a file containing youtube id (one per line)"""
     click.echo(f'Converting Youtube Video (from file) to Anchor FM')
     ids = []
     with open(filename) as file:
         for row in file:
             ids.append(row.rstrip("\n"))
-    print(ids)
+
+    convert_youtube_to_podcast(ids, **ctx.obj)
 
 @cli.command()
-def youtube_channel(draft_mode, thumbnail, add_url, is_explicit):
+@click.pass_context
+@click.argument('channel-id')
+def youtube_channel(ctx, channel_id):
     """Takes in a YouTube Channel ID"""
     click.echo('Converting YouTube Channel to Anchor FM')
+    channel_vids = return_channel(channel_id)
+    convert_youtube_to_podcast(channel_vids, **ctx.obj)
+
 
 @cli.command()
-def youtube_playlist(draft_mode, thumbnail, add_url, is_explicit):
+@click.pass_context
+def youtube_playlist(ctx, playlist_id):
     """Take in a YouTube Playlist ID"""
     click.echo('Converting YouTube Playlist to Anchor FM')
+    playlist_vids = return_playlist(playlist_id)
+    convert_youtube_to_podcast(playlist_vids, **ctx.obj)
 
 if __name__ == '__main__':
     cli()
