e2bf451cb0517d1f25bc66c413a99bb338991275
Abe <abe@habet.dev>
2022-07-20 15:59:31 -0400

    bump up version 0.1.0

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 setup (
 	name = 'tube-cast',
 	description = 'A command-line tool that converts YouTube videos into Anchor FM podcasts.',
-	version = '0.0.7',
+	version = '0.1.0',
 	install_requires = [
 	    'click',
 	    'scrapetube',
 	    'yt_dlp',
 	    'pyppeteer',
 	    ],
-	python_requires='>=3.7', # any python greater than 2.7
+	python_requires='>=3.7', # any python greater than 3.7
 	entry_points='''
 	       [console_scripts]
 	       tube-cast=src.__main__:cli
@@ -29,7 +29,7 @@ setup (
 	long_description_content_type="text/markdown",
 	license='MIT',
 	url='https://github.com/abe-101/tube-cast',
-	download_url='https://github.com/abe-101/tube-cast/releases/download/v0.0.7/tube-cast-0.0.7.tar.gz',
+	download_url='https://github.com/abe-101/tube-cast/releases/download/v0.1.0/tube-cast-0.1.0.tar.gz',
     author_email='abe@habet.dev',
     classifiers=[
         "Programming Language :: Python :: 3",
